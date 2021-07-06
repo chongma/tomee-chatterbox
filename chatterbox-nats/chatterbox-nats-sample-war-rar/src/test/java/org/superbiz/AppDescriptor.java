3f75c7420cf9f52ca33f5cf4fe0b11a5b6a2d4ef
Matthew Broadhead <nbmlaw@gmail.com>
2021-07-06 12:44:38 +0200

    trying to mock resourceadapter

--- chatterbox-nats/chatterbox-nats-sample-war-rar/src/test/java/org/superbiz/AppDescriptor.java
+++ chatterbox-nats/chatterbox-nats-sample-war-rar/src/test/java/org/superbiz/AppDescriptor.java
@@ -43,6 +43,7 @@ import org.apache.openejb.testing.Descriptors;
 import org.apache.openejb.testing.Module;
 import org.apache.openejb.testing.RandomPort;
 import org.apache.openejb.testing.SimpleLog;
+import org.apache.tomee.chatterbox.nats.adapter.NATSResourceAdapter;
 import org.apache.tomee.chatterbox.nats.adapter.out.NATSConnectionFactoryImpl;
 import org.apache.tomee.chatterbox.nats.adapter.out.NATSConnectionImpl;
 import org.apache.tomee.chatterbox.nats.adapter.out.NATSManagedConnectionFactory;
@@ -66,16 +67,17 @@ public class AppDescriptor {
     public Connector connector() {
         final ConnectionDefinition connectionDefinition = new ConnectionDefinition();
         connectionDefinition.setId("NATSConnectionFactory");
-        connectionDefinition.setConnectionImplClass(MyCon.class.getName());
-        connectionDefinition.setConnectionInterface(MyConAPI.class.getName());
-        connectionDefinition.setConnectionFactoryImplClass(MyMcf.class.getName());
-        connectionDefinition.setConnectionFactoryInterface(ConnectionFactory.class.getName());
-        connectionDefinition.setManagedConnectionFactoryClass(MyMcf.class.getName());
+        connectionDefinition.setConnectionImplClass(NATSConnectionImpl.class.getName());
+        connectionDefinition.setConnectionInterface(NATSConnection.class.getName());
+        connectionDefinition.setConnectionFactoryImplClass(NATSConnectionFactoryImpl.class.getName());
+        connectionDefinition.setConnectionFactoryInterface(NATSConnectionFactory.class.getName());
+        connectionDefinition.setManagedConnectionFactoryClass(NATSManagedConnectionFactory.class.getName());
 
         final OutboundResourceAdapter out = new OutboundResourceAdapter();
         out.getConnectionDefinition().add(connectionDefinition);
 
         final ResourceAdapter ra = new ResourceAdapter();
+        ra.setId("NATS");
         ra.setResourceAdapterClass(MyRa.class.getName());
         ra.setOutboundResourceAdapter(out);
 
@@ -89,200 +91,227 @@ public class AppDescriptor {
 		return base;
 	}
 	
-	public static class MyRa implements javax.resource.spi.ResourceAdapter {
-        @Override
-        public void start(final BootstrapContext ctx) throws ResourceAdapterInternalException {
-            // no-op
-        }
+	public static class MyRa extends NATSResourceAdapter {
+      @Override
+      public void start(final BootstrapContext ctx) throws ResourceAdapterInternalException {
+          // no-op
+      }
 
-        @Override
-        public void stop() {
-            // no-op
-        }
+      @Override
+      public void stop() {
+          // no-op
+      }
 
-        @Override
-        public void endpointActivation(final MessageEndpointFactory endpointFactory, final ActivationSpec spec) throws ResourceException {
-            // no-op
-        }
+      @Override
+      public void endpointActivation(final MessageEndpointFactory endpointFactory, final ActivationSpec spec) throws ResourceException {
+          // no-op
+      }
 
-        @Override
-        public void endpointDeactivation(final MessageEndpointFactory endpointFactory, final ActivationSpec spec) {
-            // no-op
-        }
+      @Override
+      public void endpointDeactivation(final MessageEndpointFactory endpointFactory, final ActivationSpec spec) {
+          // no-op
+      }
 
-        @Override
-        public XAResource[] getXAResources(final ActivationSpec[] specs) throws ResourceException {
-            return new XAResource[0];
-        }
-    }
-
-    public static class MyCf implements ConnectionFactory {
-        private final ConnectionManager mgr;
-        private final ManagedConnectionFactory mcf;
-
-        public MyCf(final MyMcf myMcf, final ConnectionManager cxManager) {
-            this.mcf = myMcf;
-            this.mgr = cxManager;
-        }
-
-        @Override
-        public Connection getConnection() throws ResourceException {
-            return MyCon.class/*impl, this is what we want to test*/.cast(mgr.allocateConnection(mcf, new ConnectionRequestInfo() {
-            }));
-        }
-
-        @Override
-        public Connection getConnection(ConnectionSpec properties) throws ResourceException {
-            return getConnection();
-        }
-
-        @Override
-        public RecordFactory getRecordFactory() throws ResourceException {
-            return null;
-        }
-
-        @Override
-        public ResourceAdapterMetaData getMetaData() throws ResourceException {
-            return null;
-        }
-
-        @Override
-        public void setReference(Reference reference) {
-
-        }
-
-        @Override
-        public Reference getReference() throws NamingException {
-            return null;
-        }
-    }
-
-    public static class MyMcf implements ManagedConnectionFactory {
-        @Override
-        public Object createConnectionFactory(final ConnectionManager cxManager) throws ResourceException {
-            return new MyCf(this, cxManager);
-        }
-
-        @Override
-        public Object createConnectionFactory() throws ResourceException {
-            return new MyCf(this, null);
-        }
-
-        @Override
-        public ManagedConnection createManagedConnection(Subject subject, ConnectionRequestInfo cxRequestInfo) throws ResourceException {
-            return new MyMC();
-        }
-
-        @Override
-        public ManagedConnection matchManagedConnections(Set connectionSet, Subject subject, ConnectionRequestInfo cxRequestInfo) throws ResourceException {
-            return null;
-        }
-
-        @Override
-        public void setLogWriter(PrintWriter out) throws ResourceException {
-
-        }
-
-        @Override
-        public PrintWriter getLogWriter() throws ResourceException {
-            return null;
-        }
-    }
-
-    public interface MyConAPI extends Connection {
-    }
-
-    public static class MyCon implements MyConAPI {
-        private final String arg;
-
-        public MyCon(String arg) {
-            this.arg = arg;
-        }
-
-        public String specific() {
-            return arg;
-        }
-
-        @Override
-        public Interaction createInteraction() throws ResourceException {
-            return null;
-        }
-
-        @Override
-        public javax.resource.cci.LocalTransaction getLocalTransaction() throws ResourceException {
-            return null;
-        }
-
-        @Override
-        public ConnectionMetaData getMetaData() throws ResourceException {
-            return null;
-        }
-
-        @Override
-        public ResultSetInfo getResultSetInfo() throws ResourceException {
-            return null;
-        }
-
-        @Override
-        public void close() throws ResourceException {
-
-        }
-    }
-
-    public static class MyMC implements ManagedConnection {
-        @Override
-        public Object getConnection(final Subject subject, final ConnectionRequestInfo cxRequestInfo) throws ResourceException {
-            return new MyCon("yes");
-        }
-
-        @Override
-        public void destroy() throws ResourceException {
-
-        }
-
-        @Override
-        public void cleanup() throws ResourceException {
-
-        }
-
-        @Override
-        public void associateConnection(Object connection) throws ResourceException {
-
-        }
-
-        @Override
-        public void addConnectionEventListener(ConnectionEventListener listener) {
-
-        }
-
-        @Override
-        public void removeConnectionEventListener(ConnectionEventListener listener) {
-
-        }
-
-        @Override
-        public XAResource getXAResource() throws ResourceException {
-            return null;
-        }
-
-        @Override
-        public LocalTransaction getLocalTransaction() throws ResourceException {
-            return null;
-        }
-
-        @Override
-        public ManagedConnectionMetaData getMetaData() throws ResourceException {
-            return null;
-        }
-
-        @Override
-        public void setLogWriter(PrintWriter out) throws ResourceException {
-
-        }
-
-        @Override
-        public PrintWriter getLogWriter() throws ResourceException {
-            return null;
-        }
-    }
+      @Override
+      public XAResource[] getXAResources(final ActivationSpec[] specs) throws ResourceException {
+          return new XAResource[0];
+      }
+	}
+	
+//	public static class MyRa implements javax.resource.spi.ResourceAdapter {
+//        @Override
+//        public void start(final BootstrapContext ctx) throws ResourceAdapterInternalException {
+//            // no-op
+//        }
+//
+//        @Override
+//        public void stop() {
+//            // no-op
+//        }
+//
+//        @Override
+//        public void endpointActivation(final MessageEndpointFactory endpointFactory, final ActivationSpec spec) throws ResourceException {
+//            // no-op
+//        }
+//
+//        @Override
+//        public void endpointDeactivation(final MessageEndpointFactory endpointFactory, final ActivationSpec spec) {
+//            // no-op
+//        }
+//
+//        @Override
+//        public XAResource[] getXAResources(final ActivationSpec[] specs) throws ResourceException {
+//            return new XAResource[0];
+//        }
+//    }
+//
+//    public static class MyCf implements ConnectionFactory {
+//        private final ConnectionManager mgr;
+//        private final ManagedConnectionFactory mcf;
+//
+//        public MyCf(final MyMcf myMcf, final ConnectionManager cxManager) {
+//            this.mcf = myMcf;
+//            this.mgr = cxManager;
+//        }
+//
+//        @Override
+//        public Connection getConnection() throws ResourceException {
+//            return MyCon.class/*impl, this is what we want to test*/.cast(mgr.allocateConnection(mcf, new ConnectionRequestInfo() {
+//            }));
+//        }
+//
+//        @Override
+//        public Connection getConnection(ConnectionSpec properties) throws ResourceException {
+//            return getConnection();
+//        }
+//
+//        @Override
+//        public RecordFactory getRecordFactory() throws ResourceException {
+//            return null;
+//        }
+//
+//        @Override
+//        public ResourceAdapterMetaData getMetaData() throws ResourceException {
+//            return null;
+//        }
+//
+//        @Override
+//        public void setReference(Reference reference) {
+//
+//        }
+//
+//        @Override
+//        public Reference getReference() throws NamingException {
+//            return null;
+//        }
+//    }
+//
+//    public static class MyMcf implements ManagedConnectionFactory {
+//        @Override
+//        public Object createConnectionFactory(final ConnectionManager cxManager) throws ResourceException {
+//            return new MyCf(this, cxManager);
+//        }
+//
+//        @Override
+//        public Object createConnectionFactory() throws ResourceException {
+//            return new MyCf(this, null);
+//        }
+//
+//        @Override
+//        public ManagedConnection createManagedConnection(Subject subject, ConnectionRequestInfo cxRequestInfo) throws ResourceException {
+//            return new MyMC();
+//        }
+//
+//        @Override
+//        public ManagedConnection matchManagedConnections(Set connectionSet, Subject subject, ConnectionRequestInfo cxRequestInfo) throws ResourceException {
+//            return null;
+//        }
+//
+//        @Override
+//        public void setLogWriter(PrintWriter out) throws ResourceException {
+//
+//        }
+//
+//        @Override
+//        public PrintWriter getLogWriter() throws ResourceException {
+//            return null;
+//        }
+//    }
+//
+//    public interface MyConAPI extends Connection {
+//    }
+//
+//    public static class MyCon implements MyConAPI {
+//        private final String arg;
+//
+//        public MyCon(String arg) {
+//            this.arg = arg;
+//        }
+//
+//        public String specific() {
+//            return arg;
+//        }
+//
+//        @Override
+//        public Interaction createInteraction() throws ResourceException {
+//            return null;
+//        }
+//
+//        @Override
+//        public javax.resource.cci.LocalTransaction getLocalTransaction() throws ResourceException {
+//            return null;
+//        }
+//
+//        @Override
+//        public ConnectionMetaData getMetaData() throws ResourceException {
+//            return null;
+//        }
+//
+//        @Override
+//        public ResultSetInfo getResultSetInfo() throws ResourceException {
+//            return null;
+//        }
+//
+//        @Override
+//        public void close() throws ResourceException {
+//
+//        }
+//    }
+//
+//    public static class MyMC implements ManagedConnection {
+//        @Override
+//        public Object getConnection(final Subject subject, final ConnectionRequestInfo cxRequestInfo) throws ResourceException {
+//            return new MyCon("yes");
+//        }
+//
+//        @Override
+//        public void destroy() throws ResourceException {
+//
+//        }
+//
+//        @Override
+//        public void cleanup() throws ResourceException {
+//
+//        }
+//
+//        @Override
+//        public void associateConnection(Object connection) throws ResourceException {
+//
+//        }
+//
+//        @Override
+//        public void addConnectionEventListener(ConnectionEventListener listener) {
+//
+//        }
+//
+//        @Override
+//        public void removeConnectionEventListener(ConnectionEventListener listener) {
+//
+//        }
+//
+//        @Override
+//        public XAResource getXAResource() throws ResourceException {
+//            return null;
+//        }
+//
+//        @Override
+//        public LocalTransaction getLocalTransaction() throws ResourceException {
+//            return null;
+//        }
+//
+//        @Override
+//        public ManagedConnectionMetaData getMetaData() throws ResourceException {
+//            return null;
+//        }
+//
+//        @Override
+//        public void setLogWriter(PrintWriter out) throws ResourceException {
+//
+//        }
+//
+//        @Override
+//        public PrintWriter getLogWriter() throws ResourceException {
+//            return null;
+//        }
+//    }
 }
